56c5f3199a51a6e9cc02facc35a6e1cfc29cc6f2
P-peng <894194651@qq.com>
2020-06-10 18:06:55 +0800

    [add] 1.工厂方法 [add] 1.抽象工厂

---
+++ src/main/java/com/ge/my/abstractFactory/AbstractFactoryTest.java
@@ -0,0 +1,175 @@
+package com.ge.my.abstractFactory;
+
+/**
+ * @author dengzhipeng
+ * @version 1.0
+ * @date 2020/6/10 0010
+ */
+public class AbstractFactoryTest {
+
+    public static void main(String[] args) {
+        AbstractFactory huaWeiFactory = new HuaWeiFactory();
+        huaWeiFactory.createPhone();
+        huaWeiFactory.createCharger();
+        huaWeiFactory.createHeadset();
+
+        System.out.println("");
+        //  --------------------  新加 小米 产品 --------------------
+        AbstractFactory xiaoMiFactory = new XiaoMiFactory();
+        xiaoMiFactory.createPhone();
+        xiaoMiFactory.createCharger();
+        xiaoMiFactory.createHeadset();
+    }
+}
+
+/**
+ * 手机 产品
+ */
+interface Phone {
+    // 声明类所需继承的共同接口，
+}
+
+/**
+ * 耳机 产品
+ */
+interface Headset{
+    // 声明类所需继承的共同接口，
+}
+
+/**
+ * 充电器 产品
+ */
+interface Charger{
+    // 声明类所需继承的共同接口，
+}
+
+/**
+ * 抽象工厂类
+ */
+interface AbstractFactory{
+    /**
+     * 创建手机
+     *
+     * @return
+     */
+    Phone createPhone();
+
+    /**
+     * 耳机 产品
+     *
+     * @return
+     */
+    Headset createHeadset();
+
+    /**
+     * 充电器 产品
+     *
+     * @return
+     */
+    Charger createCharger();
+}
+
+/**
+ * 华为手机
+ */
+class HuaWeiPhone implements Phone{
+
+    public HuaWeiPhone(){
+        System.out.println("华为手机创建了");
+    }
+
+}
+
+/**
+ * 华为耳机
+ */
+class HuaWeiHeadset implements Headset{
+
+    public HuaWeiHeadset(){
+        System.out.println("华为耳机创建了");
+    }
+
+}
+
+/**
+ * 华为充电器
+ */
+class HuaWeiCharger implements Charger{
+
+    public HuaWeiCharger(){
+        System.out.println("华为充电器创建了");
+    }
+}
+
+/**
+ * 华为工厂
+ */
+class HuaWeiFactory implements AbstractFactory{
+
+    public Phone createPhone() {
+        return new HuaWeiPhone();
+    }
+
+    public Headset createHeadset() {
+        return new HuaWeiHeadset();
+    }
+
+    public Charger createCharger() {
+        return new HuaWeiCharger();
+    }
+}
+
+/**
+ *  --------------------  新加 小米族 产品 --------------------
+ *  则需要添加 小米族 产品 和 小米工厂
+ */
+
+/**
+ * 小米手机
+ */
+class XiaoMiPhone implements Phone{
+
+    public XiaoMiPhone(){
+        System.out.println("小米手机创建了");
+    }
+
+}
+
+/**
+ * 小米耳机
+ */
+class XiaoMiHeadset implements Headset{
+
+    public XiaoMiHeadset(){
+        System.out.println("小米耳机创建了");
+    }
+
+}
+
+/**
+ * 小米充电器
+ */
+class XiaoMiCharger implements Charger{
+
+    public XiaoMiCharger(){
+        System.out.println("小米充电器创建了");
+    }
+}
+
+/**
+ * 小米工厂
+ */
+class XiaoMiFactory implements AbstractFactory{
+
+    public Phone createPhone() {
+        return new XiaoMiPhone();
+    }
+
+    public Headset createHeadset() {
+        return new XiaoMiHeadset();
+    }
+
+    public Charger createCharger() {
+        return new XiaoMiCharger();
+    }
+}
---
+++ src/main/java/com/ge/my/factoryMehtod/FactoryMethodTest.java
@@ -0,0 +1,128 @@
+package com.ge.my.factoryMehtod;
+
+/**
+ * 工厂方法设计模式
+ *
+ * @author dengzhipeng
+ * @version 1.0
+ * @date 2020/6/10 0010
+ */
+public class FactoryMethodTest {
+
+    public static void main(String[] args) {
+        // 华为工厂创建华为产品
+        Factory huaWeiFactory = new HuaWeiFactory();
+        huaWeiFactory.createProduct();
+
+        // 小米工厂创建小米产品
+        Factory xiaoMiFactory = new XiaoMiFactory();
+        xiaoMiFactory.createProduct();
+
+
+        //  --------------------  新加 荣耀 产品 --------------------
+        Factory rongYaoFactory = new RongYaoFactory();
+        rongYaoFactory.createProduct();
+    }
+}
+
+
+/**
+ * 产品类，也可以是抽象类
+ */
+interface Product{
+    //声明类所需继承的共同接口，
+}
+
+/**
+ * 工厂类
+ */
+interface Factory{
+    /**
+     * 创建产品
+     *
+     * @return
+     */
+    Product createProduct();
+}
+
+/**
+ * 华为产品
+ */
+class HuaWeiProduct implements Product{
+    public HuaWeiProduct(){
+        System.out.println("华为产品 创建");
+    }
+}
+
+/**
+ * 华为工厂类
+ *
+ */
+class HuaWeiFactory implements Factory{
+    /**
+     * 华为 创建产品
+     *
+     * @return
+     */
+    public Product createProduct() {
+        return new HuaWeiProduct();
+    }
+}
+
+/**
+ * 小米产品
+ *
+ */
+class XiaoMiProduct implements Product{
+    public XiaoMiProduct(){
+        System.out.println("小米产品 创建");
+    }
+}
+
+/**
+ * 小米工厂类
+ *
+ */
+class XiaoMiFactory implements Factory{
+    /**
+     * 小米 创建产品
+     *
+     * @return
+     */
+    public Product createProduct() {
+        return new XiaoMiProduct();
+    }
+}
+
+/**
+ *  --------------------  新加 荣耀 产品 --------------------
+ *  则需要添加 荣耀产品 和 荣耀工厂
+ */
+
+/**
+ * 荣耀产品
+ *
+ */
+class RongYaoProduct implements Product{
+    public RongYaoProduct(){
+        System.out.println("荣耀产品 创建");
+    }
+}
+
+/**
+ * 荣耀工厂类
+ *
+ */
+class RongYaoFactory implements Factory{
+    /**
+     * 荣耀 创建产品
+     *
+     * @return
+     */
+    public Product createProduct() {
+        return new RongYaoProduct();
+    }
+}
+
+
+
